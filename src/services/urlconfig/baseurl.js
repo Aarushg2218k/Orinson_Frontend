8f693c0c964a3d3a16e8e0d35a8deb3cd4493b16
Aarush goyal <118333487+Aarushg2218k@users.noreply.github.com>
2024-09-01 12:54:27 +0530

    hii

--- src/services/urlconfig/baseurl.js
+++ src/services/urlconfig/baseurl.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL="http://localhost:2002/";
+// const baseURL="http://localhost:2002/";
+const baseURL="https://orinson-backend.onrender.com/";
 
 const publicAxios = axios.create({baseURL});
 
